4911a05f4dfc5e26a0d2cabd1cb9e736f9f625ee
victoriaovcharova <victoria.ovcharova93@gmail.com>
2024-03-27 21:08:28 +0200

    1

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
 // Відкриття вікна меню
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[backdrop-menu-open]'),
+    closeModalBtn: document.querySelector('[backdrop-menu-close]'),
+    modal: document.querySelector('[backdrop-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -20,9 +20,9 @@ document.getElementById('toggleMenu').addEventListener('click', function () {
 });
 
 // Закриття вікна меню при натисканні на пункт навігації
-var menuLinks = document.querySelectorAll('.menu-nav-link');
+var menuLinks = document.querySelectorAll('.backdrop-menu-list');
 var menu = document.querySelector('.menu');
-var closeButton = document.querySelector('.menu-close-btn');
+var closeButton = document.querySelector('.modal-menu-close-button');
 function closeMenu() {
   menu.classList.remove('is-open');
 }
